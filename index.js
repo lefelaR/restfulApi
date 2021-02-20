d82608ef27f3d81875d80835959ac9819d1b01e1
lefelaR <rakgew@gmail.com>
2021-02-20 07:12:32 +0200

    cool

--- index.js
+++ index.js
@@ -4,8 +4,37 @@ const { request } = require("express");
 
 // const app = require('express') this connects to the express module, but you have to openn it ip once that is done
 
-const app = require('express')();
+const express = require('express');
+const app = express();
 const PORT = 8080;
+
+
+app.use( express.json());
+
+
+app.get('/tshirt',(req, res)=>{
+    res.status(200).send({
+        tshirt:'👕',
+        size: 'larger',
+        style: 'slimfit'
+    })
+});
+
+app.post(
+    '/tshirt/:id',(req, res)=>{
+     const {id } = req.params;  
+     const {logo} = req.body;
+
+     if(!logo){
+         res.status(418).send({message: 'We require a logo from you in the body'})
+     }
+
+     res.send({
+         tshirt: `👕 with your ${logo} and ID of ${id}`,
+     });
+
+
+    })
 
 
 app.listen(
